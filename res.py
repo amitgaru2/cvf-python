93cc772e64a013f21e72fd34d762fe4db83d7ec8
Amit Garu <amitgaru2@gmail.com>
2024-10-26 15:51:22 -0600

    updates

--- res.py
+++ res.py
@@ -1,8 +1,25 @@
+class Rank:
+    def __init__(self, L, C, M):
+        self.L = L
+        self.C = C
+        self.M = M
+
+    def add_cost(self, val):
+        self.L += val
+        self.C += 1
+        self.M = max(val, self.M)
+
+    def __str__(self) -> str:
+        return f"L: {self.L}, C: {self.C}, M: {self.M}"
+
+    __repr__ = __str__
+
+
 class TreeNode:
     def __init__(self, config, children: set["TreeNode"] = set()) -> None:
         self.config = config
         self.children = children
-        self.cost = []
+        self.cost = Rank(0, 0, 0)
 
     def traverse(self):
         queue = [self]
@@ -44,7 +61,9 @@ def dfs(path):
 def backtrack_path(path: list[TreeNode]):
     for i, node in enumerate(path):
         # node.cost = (i + node.cost) / 2 if node.cost is not None else i
-        node.cost.append(i)
+        # node.cost.L += i
+        node.cost.add_cost(i)
+        # node.cost.append(i)
 
 
 invariants = {(0, 1, 0)}
